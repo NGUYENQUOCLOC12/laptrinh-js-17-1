4fe1b26e1de15e02dbe15a5d5aaacbfdf3cd9282
Your Name <you@example.com>
2024-03-06 14:31:13 +0700

    Bài tập

---
+++ 6-3/This.js
@@ -0,0 +1,15 @@
+var student = {
+
+    name: 'Mai ha Thi',
+    
+    getName: function() {
+    
+    console.log(this.name);
+    
+    return this.name;
+    
+    }
+    
+    };
+    
+    var _deStudent = student.getName();
